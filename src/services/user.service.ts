a9fa2953c2796ab848f0a76de320540cfbac2d93
zeinirfansyah <12.zeinirfansyah@gmail.com>
2024-10-08 05:38:24 +0700

    refactor [user.services.ts]: removed existing file check condition

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,22 +1,8 @@
-import path from 'path'
-import fs from 'fs'
 import { createUserRepository } from "../repositories/user.repository";
 import IUserType from "../types/user.types";
 import bcrypt from 'bcryptjs'
 
 export const createUserService = async (payload: Omit<IUserType, 'created_at' | 'updated_at'>): Promise<IUserType> => {
-
-    if (payload.profile_image) {
-        const existingProfileImage = path.join(
-            __dirname,
-            `../../public${payload.profile_image}`
-        );
-
-        if (fs.existsSync(existingProfileImage)) {
-            fs.unlinkSync(existingProfileImage);
-        }
-    }
-
     const hashedPassword = await bcrypt.hash(payload.password, 10)
 
     const userData: IUserType = {
