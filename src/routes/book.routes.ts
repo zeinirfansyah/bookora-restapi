50cab0bf5572a78a6addd859c70b86e1ca778a26
zeinirfansyah <12.zeinirfansyah@gmail.com>
2024-10-29 08:43:52 +0700

    refactor [book.routes]: implemented validateBookInput

--- src/routes/book.routes.ts
+++ src/routes/book.routes.ts
@@ -1,9 +1,10 @@
 import { Router } from 'express'
 import { createBook, getBooks } from '../controllers/book.controller'
+import { validateBookInput } from '../middlewares/validators/book.validator'
 
 const bookRoute = Router()
 
 bookRoute.get('/book', getBooks)
-bookRoute.post('/book', createBook)
+bookRoute.post('/book', validateBookInput, createBook)
 
 export default bookRoute
