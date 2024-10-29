e33e6211ff3aec5de88cb0720092d7b268131ec5
zeinirfansyah <12.zeinirfansyah@gmail.com>
2024-10-29 08:43:08 +0700

    refactor [book.controller]: handle publisher_code and author_code req body

--- src/controllers/book.controller.ts
+++ src/controllers/book.controller.ts
@@ -3,13 +3,15 @@ import { getBookCategoryService } from "../services/book_category.service";
 import { createBookService, getAllBooksService } from "../services/book.service";
 import { UploadedFile } from "express-fileupload";
 import { uploadFile } from "../helpers/upload";
+import { getBookPublisherService } from "../services/book_publisher.service";
+import { getBookAuthorRepository } from "../repositories/book_author.repository";
 
 export const createBook = async (req: Request, res: Response): Promise<void> => {
     const {
         book_name,
-        book_category,
-        // book_publisher,
-        // book_author,
+        category_code,
+        publisher_code,
+        author_code,
     } = req.body;
 
     const book_cover = req.files?.book_cover as UploadedFile
@@ -29,17 +31,17 @@ export const createBook = async (req: Request, res: Response): Promise<void> => 
         }
 
         let book_category_id
-        // let book_publisher_id
-        // let book_author_id
+        let book_publisher_id
+        let book_author_id
 
-        if (book_category) {
-            const category = await getBookCategoryService({ category_name: book_category })
+        if (category_code) {
+            const category = await getBookCategoryService({ category_code: category_code })
 
             if (!category) {
                 res.status(400).json({
                     success: false,
                     status: 400,
-                    message: `Category '${book_category}' does not exist.`,
+                    message: `Category with code '${category_code}' does not exist.`,
                 });
                 return;
             }
@@ -47,13 +49,43 @@ export const createBook = async (req: Request, res: Response): Promise<void> => 
             book_category_id = category?.id || null
         }
 
+        if (publisher_code) {
+            const publisher = await getBookPublisherService({ publisher_code: publisher_code })
+
+            if (!publisher) {
+                res.status(400).json({
+                    success: false,
+                    status: 400,
+                    message: `Publisher with code '${publisher_code}' does not exist.`,
+                });
+                return;
+            }
+
+            book_publisher_id = publisher?.id || null
+        }
+
+        if (author_code) {
+            const author = await getBookAuthorRepository({ author_code: author_code })
+
+            if (!author) {
+                res.status(400).json({
+                    success: false,
+                    status: 400,
+                    message: `Author with code '${author_code}' does not exist.`,
+                });
+                return;
+            }
+
+            book_author_id = author?.id || null
+        }
+
 
         const newBook = await createBookService({
             book_code: `USR_${new Date().getTime()}`,
             book_name,
             book_category_id,
-            // book_publisher_id,
-            // book_author_id,
+            book_publisher_id,
+            book_author_id,
             book_cover: uploadedBookCover
         });
 
