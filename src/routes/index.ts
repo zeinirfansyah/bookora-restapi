bd666c8caea37b3fa7a798d97be099e21d8d3057
zeinirfansyah <12.zeinirfansyah@gmail.com>
2024-11-06 06:56:13 +0700

    feat [account]: added get account data (authenticated user)

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,9 +2,11 @@ import { Router } from 'express'
 import userRouter from './user.routes'
 import bookRoute from './book.routes'
 import authRouter from './auth.routes'
+import accountRouter from './account.routes'
 
 const appRouter = Router()
 
+appRouter.use('/api', accountRouter)
 appRouter.use('/api', userRouter)
 appRouter.use('/api', bookRoute)
 appRouter.use('/api/auth', authRouter)
