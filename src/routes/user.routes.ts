54eb2e8110ef56a220b0cd54b71b66aaba80f162
zeinirfansyah <12.zeinirfansyah@gmail.com>
2024-11-06 06:55:56 +0700

    refactor [user.routes]: implemented authentication and authorization

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,14 +1,15 @@
 import { createUser, deleteUser, deleteUsers, getUser, getUsers, updateUser } from '../controllers/user.controller'
 import { Router } from 'express'
 import { validateUserInput } from '../middlewares/validators/user.validator'
+import { authorizeRole, verifyToken } from '../middlewares/auth'
 
 const userRouter = Router()
 
-userRouter.post('/user', validateUserInput, createUser)
+userRouter.post('/user', verifyToken, authorizeRole("ADMIN"), validateUserInput, createUser)
 userRouter.patch('/user/:user_code', validateUserInput, updateUser)
 userRouter.get('/user/:user_code', getUser)
-userRouter.get('/user', getUsers)
+userRouter.get('/user', verifyToken, authorizeRole("ADMIN"), getUsers)
 userRouter.delete('/user/:user_code', deleteUser)
-userRouter.delete('/user', deleteUsers)
+userRouter.delete('/user', verifyToken, authorizeRole("ADMIN"), deleteUsers)
 
 export default userRouter
