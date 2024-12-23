4594a4fa183465555084cbb643d8813aa095d7df
zeinirfansyah <12.zeinirfansyah@gmail.com>
2024-12-23 23:07:01 +0700

    feat [author validator]: added author input validator

--- src/routes/book_author.routes.ts
+++ src/routes/book_author.routes.ts
@@ -2,10 +2,11 @@ import { Router } from 'express'
 import { getAuthors } from '../controllers/book_author_controllers/get_all_book_author.controller'
 import { createAuthor } from '../controllers/book_author_controllers/create_book_author.controller'
 import { authorizeRole, verifyToken } from '../middlewares/auth'
+import { validateAuthorInput } from '../middlewares/validators/book_author.validator'
 
 const authorRoute = Router()
 
 authorRoute.get('/book-author', getAuthors)
-authorRoute.post('/book-author', verifyToken, authorizeRole('ADMIN'), createAuthor)
+authorRoute.post('/book-author', verifyToken, authorizeRole('ADMIN'), validateAuthorInput, createAuthor)
 
 export default authorRoute
